a68f62e712a7487a93d61e98ef37a30e4428e12c
Redjard <github@julianahl.de>
2023-01-17 23:50:14 +0100

    small rewrites, remove scheduler
    removed scheduler as it was overkill. Wrote own cron instead which is more lightweight (still kinda overkill)
    Also sped up small parts by utilizing async better (unawait)

--- main.py
+++ main.py
@@ -2,12 +2,9 @@ import nextcord, nextcord.ext.commands
 from sqlite3 import connect 
 
 import time
+from asyncio import create_task as unawait
 import re  # regex
 from typing import Optional
-# strlen = lambda string: len(string.encode('utf-8'))
-strlen = lambda string: len(string)  # oops, dc actually uses unicode char counts
-import schedule
-import threading
 
 from env import *  # api keys
 from settings import *
@@ -15,19 +12,20 @@ from settings import *
 log = print
 if not DEBUG: log = lambda *_: None  # disable log on release
 
+############# CLIENT INITIALIZATION #############
 
 intents = nextcord.Intents.default()
 intents.message_content = True
 intents.members = True
+intents.typing = False
 
-client = nextcord.ext.commands.Bot(command_prefix='!', intents=intents)
+log(list(intents))
+client = nextcord.ext.commands.Bot( command_prefix='!', intents=intents, default_guild_ids=BOT_SERVER_IDS )
 
+############# LIBRARY #############
 
-database = connect("sqlite.db")
-
-usr_cooldowns = {}
-
-
+# in case dc ever changes how they measure stringlength AGAIN
+def dcStrlen(string): return len(string)
 
 # get xp, level, ... of user_id, and create user entry if it doesn't exist
 def get_userlevel(user_id):
@@ -57,43 +55,23 @@ def get_user_rank(xp,level):
   return rank
 
 async def getUserName(user_id):
-  if check_snitchtome(user_id):
-    username = f'<@{user_id}>'
-  elif user := await get_user(user_id):
-    username = f'`{user}`'
-  else:
-    username = f'`<@{user_id}>`'
-  return username
-
-async def get_user(user_id):
   user_id = int(user_id)  # you would not believe how many issues this caused, I wasted 3 hours on this shit. Fuck soft typing, fuck nextcord, fuck sqlite
   
-  # get from cache
-  user = client.get_user(user_id)
-  
-  # get fresh
-  if not user:
-    try:
-      log(f'trying to fetch unknown user <@{user_id}>')
-      user = await client.fetch_user(user_id)
-    except: pass
-  
-  return user
-
-async def get_channel(channel_id):
-  channel_id = int(channel_id)  # jsut to make sure after the get_user() disaster
+  if check_snitchtome(user_id):
+    return f'<@{user_id}>'
   
   # get from cache
-  channel = client.get_channel(channel_id)
+  if client.get_user(user_id):
+    return f'`{client.get_user(user_id)}`'
   
   # get fresh
-  if not channel:
-    try:
-      log(f'trying to fetch unknown channel {channel_id}')
-      channel = await client.fetch_channel(channel_id)
-    except: pass
+  try:
+    log(f'trying to fetch unknown user <@{user_id}>')
+    return f'`{await client.fetch_user(user_id)}`'
+  except:
+    log(f'failed to fetch unknown user <@{user_id}>')
+    return f'`<@{user_id}>`'
   
-  return channel
 
 def parseIntInput(num,default,min=None,max=None):
   try: num = int(num)
@@ -157,14 +135,14 @@ async def get_leaderboard_msg( page: int, pagesize: int, is_xp_leaderboard: bool
     else:
       row = f"`Rank {rank} - Level {intToWidth(level,width_level)} ({intToWidth(xp,width_xp)}/{intToWidth(LEVELUP_XP(level),width_lvlupxp)}):` {await getUserName(user_id)}\n"
     
-    if strlen(chunks[-1]+row) > DISCORD_CHAR_LIMIT:  # this row would make message too long, start new block
+    if dcStrlen(chunks[-1]+row) > DISCORD_CHAR_LIMIT:  # this row would make message too long, start new block
       chunks.append("")
     
     chunks[-1] += row
   
   return chunks
 
-@client.slash_command(guild_ids=BOT_SERVER_IDS, description="List the top ranked (most 'active') users")
+@client.slash_command(description="List the top ranked (most 'active') users")
 async def leaderboard(message:  nextcord.Interaction,
   page: int = nextcord.SlashOption(name="page", required=False, default=1, min_value=1),
   pagesize: int = nextcord.SlashOption(name="pagesize", required=False, default=10, min_value=1, max_value=MAX_LEADERBOARD_SIZE),
@@ -192,7 +170,7 @@ async def leaderboard( message: nextcord.Interaction, page = None, pagesize = No
 
 ############# XPLEADERBOARD COMMAND #############
 
-@client.slash_command(guild_ids=BOT_SERVER_IDS, description="List the top ranked (most 'active') users")
+@client.slash_command(description="List the top ranked (most 'active') users")
 async def xpleaderboard(message:  nextcord.Interaction,
   page: int = nextcord.SlashOption(name="page", required=False, default=1, min_value=1),
   pagesize: int = nextcord.SlashOption(name="pagesize", required=False, default=10, min_value=1, max_value=MAX_LEADERBOARD_SIZE),
@@ -230,7 +208,7 @@ async def get_rank_msg( user_id: int ) -> str:
   
   return f"User {await getUserName(user_id)} is at rank {rank} with level {level} ({xp}/{LEVELUP_XP(level)} XP)"
 
-@client.slash_command(guild_ids=BOT_SERVER_IDS, description='Get xp and level of yourself or another user')
+@client.slash_command(description='Get xp and level of yourself or another user')
 async def rank(message:  nextcord.Interaction, username: Optional[nextcord.Member] = nextcord.SlashOption(name="user", required=False, description='set to query for user, or leave empty')) -> None:
   log(f'/rank {username}')
   
@@ -277,7 +255,7 @@ async def get_xp_msg( user_id: int ) -> str:
   
   return f"User {await getUserName(user_id)} is at rank {rank} with {totalxp} XP"
 
-@client.slash_command(guild_ids=BOT_SERVER_IDS, description='Get total XP of yourself or another user')
+@client.slash_command(description='Get total XP of yourself or another user')
 async def xp(message:  nextcord.Interaction, username: Optional[nextcord.Member] = nextcord.SlashOption(name="user", required=False, description='set to query for user, or leave empty')) -> None:
   log(f'/xp {username}')
   
@@ -327,7 +305,7 @@ async def do_pingme( user_id: int ) -> str:
     database.execute( "INSERT INTO ping_users VALUES (?)", (user_id,) )
     return "you will now get pinged upon levelup"
 
-@client.slash_command(guild_ids=BOT_SERVER_IDS, description='Toggle getting pinged when you level up (default is off)')
+@client.slash_command(description='Toggle getting pinged when you level up (default is off)')
 async def pingme( message: nextcord.Interaction ) -> None:
   log(f'/pingme {message.user}')
   
@@ -359,7 +337,7 @@ async def toggle_snitchtome( user_id: int ) -> str:
     database.execute( "INSERT INTO snitchtome_users VALUES (?)", (user_id,) )
     return "I will now snitch to you when someone queries your rank"
 
-@client.slash_command(guild_ids=BOT_SERVER_IDS, description='Toggle getting pinged when someone queries your rank (default is off)')
+@client.slash_command(description='Toggle getting pinged when someone queries your rank (default is off)')
 async def snitchtome( message: nextcord.Interaction ) -> None:
   log(f'/snitchtome {message.user}')
   
@@ -375,32 +353,32 @@ async def snitchtome( message: nextcord.Interaction ) -> None:
   
   await message.reply(msg)
 
-############# XP LISTENER #############
+############# XP LISTENER AND OTHER MESSAGE LISTENERS #############
 
+usr_cooldowns = {}
 @client.listen('on_message')
 async def msg(message):
-  await operationCounterEEP(message)
-  
   author = message.author
 
   # if author.bot: return  # ignore bots
+  
   if usr_cooldowns.get(author.id, 0.0) > time.time():
     log(f'skipped msg by {author.name} due to timeout ({usr_cooldowns.get(author.id, 0.0) - time.time()}s remaining)')
     return # check cooldown
-
-  xp, level = get_userlevel(author.id)
-
-  xp += XP_GAIN_AMOUNT()
+  usr_cooldowns[author.id] = time.time() + TIMEOUT
   
+  
+  xp, level = get_userlevel(author.id)
+  
+  xp += XP_GAIN_AMOUNT()
   leveledup = xp >= LEVELUP_XP(level)
+  
   if leveledup:
     xp -= LEVELUP_XP(level)  # say levelup is 1000 xp and user has 1005 xp, they should still have 5 xp after levelup
     level += 1
-    leveledup = True
   
-  
-  usr_cooldowns[author.id] = time.time() + TIMEOUT
   database.execute("UPDATE users SET xp = ?, level = ? WHERE id = ?;", (xp, level, str(author.id)))
+  
   
   log(f'got msg by {author} (xp: {xp}, lvl: {level})')
   
@@ -411,14 +389,11 @@ async def msg(message):
     
     username = f'<@{author.id}>' if pinguser(author.id) else f'`{author}`'  # ping if set to do so
     
-    await (await get_channel(BOT_CHANNEL_ID)).send(f"{username} leveled up to {level}!!  They are currently ranked {rank}")
+    unawait( client.get_partial_messageable(BOT_CHANNEL_ID).send(f"{username} leveled up to {level}!!  They are currently ranked {rank}") )
     
     print(f'{author} leveled up! (xp: {xp}, lvl: {level})')
-  
-  if commitEvent.is_set():
-    database.commit()
-    commitEvent.clear()
 
+@client.listen('on_message')
 async def operationCounterEEP(message):
   eep = f'{chr(0x6d)}eep'  # the accursed word
   allowedEEPpercent = 0.15
@@ -429,7 +404,6 @@ async def operationCounterEEP(message):
   
   content = message.content
   content = re.sub("<:"+eep+":\\d{10,}>", eep, content)  # replace eep emojis with normal eeps for more realistic evaluation
-  log(content,message.content)
   
   # only affect messages that are primarily (>20%) eeps
   if not len(content) < 4/allowedEEPpercent * content.lower().count( eep ):
@@ -442,8 +416,41 @@ async def operationCounterEEP(message):
   await message.add_reaction(emoji)
   log('Shut')
 
+############# CRON #############
 
-# database.execute("DROP TABLE android_metadata;")  # clean up after my android sqlite editor
+def prune_timeouts():
+  global usr_cooldowns  # do atomic swap of dict to prevent thread issues
+  usr_cooldowns = { id: cooldowntime for id, cooldowntime in usr_cooldowns.items() if cooldowntime > time.time() }  # only keep timeouts in the future
+
+# minimum frequency is 1 minute
+cronjobs = [
+  { 'name': "db autosave",        'frequencySeconds':  300, 'nextrun': 0, 'function': lambda:database.commit() },
+  { 'name': "clear old timeouts", 'frequencySeconds': 3600, 'nextrun': 0, 'function': prune_timeouts },
+]
+
+@client.listen('on_ready')
+async def cron():
+  print('Ready. Starting internal Cron')
+  
+  while not client.is_closed():  # check is_closed in case we missed the on_close event
+    
+    for cronjob in cronjobs:
+      if cronjob['nextrun'] <= time.time():
+        cronjob['nextrun'] = time.time() + cronjob['frequencySeconds']
+        cronjob['function']()
+        log(f"ran {cronjob['name']}")
+    
+    try:
+      if not client.is_closed(): await client.wait_for( 'close', timeout=60 )
+      break
+    except: pass
+  
+  print('Stopping internal Cron')
+
+############# DATABASE #############
+
+database = connect("sqlite.db")
+
 database.execute("""CREATE TABLE IF NOT EXISTS
   users (
     id varchar(400),
@@ -463,38 +470,9 @@ database.execute("""CREATE TABLE IF NOT EXISTS
 """)
 # database.execute("UPDATE users SET xp = 90, level = 0 WHERE id = 933496023916093502;")  # my testing account (Greenjard)
 
+############# STARTUP AND SHUTDOWN #############
 
-def prune_timeouts():
-  global usr_cooldowns  # do atomic swap of dict to prevent thread issues
-  usr_cooldowns = { id: cooldowntime for id, cooldowntime in usr_cooldowns.items() if cooldowntime > time.time() }  # only keep timeouts in the future
-
-commitEvent = threading.Event()
-schedule.every(5).minutes.do( lambda: commitEvent.set() )
-schedule.every(60).minutes.do( prune_timeouts )
-# schedule.every(5).seconds.do( lambda: print('eep.') )  # eep. (great for testing)
-
-exitEvent = threading.Event()
-schedulerStoppedEvent = threading.Event()
-
-def checkTime():
-  while True:
-    for _ in range(100):
-      if exitEvent.is_set():
-        schedulerStoppedEvent.set()
-        return
-      time.sleep(0.1)
-    schedule.run_pending()
-threading.Thread(target=checkTime).start()
-
-
-
-try:
-  client.run(BOT_API_KEY)
-except KeyboardInterrupt:  # should not be called because client.run handles it by itself without throwing an error
-  print('exiting due to keyboard interrupt')
-
-exitEvent.set()  # signal thread to sudoku
-schedulerStoppedEvent.wait(3)  # wait for thread to sudoku
+client.run(BOT_API_KEY)
 
 database.commit()
 database.close()
